be451367a9ae798a4e49525cf6cfbf36f149db40
ATIK <atikul2585@gmail.com>
2026-06-22 19:01:47 +0600

    feat: Added og and no indexes page.

--- src/components/serviceCore/ServiceCorePage.jsx
+++ src/components/serviceCore/ServiceCorePage.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import ServiceCoreBrands from "./ServiceCoreBrands";
 import ServiceCoreDifference from "./ServiceCoreDifference";
 import ServiceCoreFaq from "./ServiceCoreFaq";
@@ -10,6 +11,34 @@ import ServiceCoreSpecialization from "./ServiceCoreSpecialization";
 import ServicveCoreTellUsAboutCampaign from "./ServicveCoreTellUsAboutCampaign";
 
 export default function ServiceCorePage() {
+  useEffect(() => {
+    const ogImageUrl = `${window.location.origin}/Socialmelo-for-Brands-and-Creators.jpg`;
+
+    const robots = document.createElement("meta");
+    robots.name = "robots";
+    robots.content = "noindex, nofollow";
+    document.head.appendChild(robots);
+
+    let ogImage = document.querySelector('meta[property="og:image"]');
+    const ogImageCreated = !ogImage;
+    const previousOgImage = ogImage?.getAttribute("content");
+    if (!ogImage) {
+      ogImage = document.createElement("meta");
+      ogImage.setAttribute("property", "og:image");
+      document.head.appendChild(ogImage);
+    }
+    ogImage.setAttribute("content", ogImageUrl);
+
+    return () => {
+      robots.remove();
+      if (ogImageCreated) {
+        ogImage.remove();
+      } else if (previousOgImage != null) {
+        ogImage.setAttribute("content", previousOgImage);
+      }
+    };
+  }, []);
+
   return (
     <>
       <ServiceCoreHero />
