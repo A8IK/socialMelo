328d3246106fbaeddac5a9fb643af8c58a5a04ee
ATIK <atikul2585@gmail.com>
2026-06-26 11:15:01 +0600

    feat: noindex, nofollow.

--- src/components/newYork/NewYorkPage.jsx
+++ src/components/newYork/NewYorkPage.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import NewWorkHero from "./NewWorkHero";
 import NewYorkBreadcrumbs from "./NewYorkBreadcrumbs";
 import NewYorkFAQ from "./NewYorkFAQ";
@@ -10,6 +11,17 @@ import NewYorkTellUsAboutCampaign from "./NewYorkTellUsAboutCampaign";
 import NewYorkWhereOurNYC from "./NewYorkWhereOurNYC";
 
 export default function NewYorkPage() {
+  useEffect(() => {
+    const robots = document.createElement("meta");
+    robots.name = "robots";
+    robots.content = "noindex, nofollow";
+    document.head.appendChild(robots);
+
+    return () => {
+      robots.remove();
+    };
+  }, []);
+
   return (
     <>
       <NewYorkBreadcrumbs />
